2b3bf93b19fcc1800cf99858794a9b411f243947
Pepokrieg <128230685+Pepokrieg@users.noreply.github.com>
2026-03-11 12:13:44 -0500

    Actualización Lógica Enemigos
    Todas las torres tienen Ballestas que disparan prefab de flecha y siguen al jugador, el jugador debe tener tag "Player" y debe tener el Layer "Player"

--- Assets/Scripts/ShootForward.cs
+++ Assets/Scripts/ShootForward.cs
@@ -1,17 +1,51 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class ShootForward : MonoBehaviour
 {
     [SerializeField] float speedProjectile = 50f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    Rigidbody _rb;
+
     void Start()
     {
-        
+        // A projectile debe tener un Rigidbody para recibir colisiones.
+        _rb = GetComponent<Rigidbody>();
+        /*if (_rb == null)
+        {
+            _rb = gameObject.AddComponent<Rigidbody>();
+            _rb.useGravity = false;
+            _rb.isKinematic = false;
+            _rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }*/
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speedProjectile * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        TryDestroyOnImpact(this.gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground") && gameObject.CompareTag("Arrow") || other.CompareTag("Buildings") && gameObject.CompareTag("Arrow") || other.CompareTag("Player") && gameObject.CompareTag("Arrow"))
+        {
+            TryDestroyOnImpact(this.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void TryDestroyOnImpact(GameObject other)
+    {
+        // Se permiten etiquetas comunes en mayúsculas/minúsculas.
+        string tag = other.tag;
+        if (tag == "Ground" || tag == "ground" || tag == "Buildings" || tag == "buildings" || tag == "Player" || tag == "player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
